1f45343eea573b5f8540005146b01fdc0acded61
ArabFullStack <eyaniyi2020@gmail.com>
2021-06-01 15:48:57 -0400

    first commit

---
+++ src/database/assessement.sql
@@ -0,0 +1,8 @@
+cd C:\Program Files\MySQL Server 8.0\bin
+start mongo.exe
+
+cd C:\Program Files\MySQL\MySQL Server 8.0\bin
+
+
+CREATE DATABASE  workers;
+
---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const url = `mongodb+srv://sample_user:<Redlobster123.>@my-sample-cluster-b3ugy.mongodb.net/<Arab>?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
