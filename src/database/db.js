79014ca9f036dcd2105b5870998eb43d4833e0d1
ArabFullStack <eyaniyi2020@gmail.com>
2021-06-02 10:00:34 -0400

    updated codes to perform better

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose')
 
-const url = `mongodb+srv://sample_user:<Redlobster123.>@my-sample-cluster-b3ugy.mongodb.net/<Arab>?retryWrites=true&w=majority`;
+const url = `mongodb+srv://Arab:Redlobster123.@cluster0.3wsaf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const connectionParams={
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true 
 }
-mongoose.connect(url,connectionParams)
+
+const dbConnect =() =>{
+    mongoose.connect(url,connectionParams)
     .then( () => {
         console.log('Connected to database ')
     })
     .catch( (err) => {
         console.error(`Error connecting to the database. \n${err}`);
     })
+}
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')   
 const app = express();               
 const port = 5000;
+const dbConnect = require('./database/db');
 
+dbConnect();
 
 
 app.listen(port, ()=>console.log(`app is listening on ${port}`));
